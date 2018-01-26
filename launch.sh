fd852fa4af5a3214b7b9d4a4ca33f9744a4911c3
Kamil Slowikowski <kslowikowski@gmail.com>
2018-01-26 15:27:41 -0500

    Update launch.sh
    Former-commit-id: a330498164f4b5581a3558d1460b93697d3ba7c1

--- launch.sh
+++ launch.sh
@@ -3,11 +3,11 @@
 src="/Users/slowikow/Dropbox/work/immunogenomics/ampviewer/"
 des="ig:/srv/shiny-server/ampviewer/"
 
-rsync -avh \
-    --omit-dir-times \
-    --exclude=make_tar.sh \
-    --exclude=launch.sh \
-    --exclude=*.Rproj \
-    --exclude=data/*.rds \
-    --exclude=.* \
-    "$src" "$des"
+rsync -rlzuvh \
+   --exclude=*.png \
+   --exclude=make_tar.sh \
+   --exclude=launch.sh \
+   --exclude=*.Rproj \
+   --exclude=data/*.rds \
+   --exclude=.* \
+   "$src" "$des"
