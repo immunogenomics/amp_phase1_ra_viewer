8e1e17830db7a878f614cfbcf0a56b76a6c1aeee
Kamil Slowikowski <kslowikowski@gmail.com>
2018-02-12 20:55:00 -0500

    use Matrix(), show memory usage
    Former-commit-id: d4f0d35264e69be20230a34beee0ad01f60c2a08

--- app.R
+++ app.R
@@ -24,41 +24,41 @@ library(viridis)
 library(egg)
 
 library(Matrix)
-library(parallel)
+# library(parallel)
 # library(formattable)
 
 #library(pheatmap)
-library(d3heatmap)
+# library(d3heatmap)
 
 meta_colors <- list(
-fine_cluster = c(
-  "CF1" = "#6BAED6",
-  "CF2" = "#08306B",
-  "CF3" = "#DEEBF7",
-  "CF4" = "grey",
-  "CT1" = "#FEB24C",
-  "CT2" = "#8C510A",
-  "CT3" = "brown",
-  "CT4" = "#FFFF33",
-  "CT5" = "#C7EAE5",
-  "CT6" = "#003C30",
-  "CT7" = "#35978F",
-  "CB1" = "#FCBBA1",
-  "CB2" = "#CB181D", #FB6A4A #A50F15
-  "CB3" = "#67000D",
-  "CB4" = "#FB9A99",
-  "CM1" = "#AE017E",
-  "CM2" = "#F768A1",
-  "CM3" = "#FDE0EF", #FCC5C0
-  "CM4" = "#49006A"
- )
+  "fine_cluster" = c(
+    "CF1" = "#6BAED6",
+    "CF2" = "#08306B",
+    "CF3" = "#DEEBF7",
+    "CF4" = "grey",
+    "CT1" = "#FEB24C",
+    "CT2" = "#8C510A",
+    "CT3" = "brown",
+    "CT4" = "#FFFF33",
+    "CT5" = "#C7EAE5",
+    "CT6" = "#003C30",
+    "CT7" = "#35978F",
+    "CB1" = "#FCBBA1",
+    "CB2" = "#CB181D", #FB6A4A #A50F15
+    "CB3" = "#67000D",
+    "CB4" = "#FB9A99",
+    "CM1" = "#AE017E",
+    "CM2" = "#F768A1",
+    "CM3" = "#FDE0EF", #FCC5C0
+    "CM4" = "#49006A"
+  )
 )
 
 # Prepare data ----------------------------------------------------------------
 
 get_markers <- function(log2cpm, clusters) {
   # Get a Wilcox p-value for each gene and each cluster. 
-  retval <- mclapply(X = unique(clusters), FUN = function(cluster) {
+  retval <- parallel::mclapply(X = unique(clusters), FUN = function(cluster) {
      ix_x <- which(clusters == cluster)
      ix_y <- which(clusters != cluster)
      pvals <- apply(log2cpm, 1, function(row) {
@@ -88,11 +88,13 @@ cell_types <- c(
 # Read 4 datasets: bcell, tcell, mono, fibro
 # Preprocess into a file for quick loading.
 data_file <- "data/shiny.rda"
-if (!file.exists(data_file)) {
+if (file.exists(data_file)) {
+  load(data_file)
+} else {
   e <- environment()
   for (cell_type in cell_types) {
     log2cpm <- readRDS(sprintf("data/%s_exp.rds", cell_type))
-    # log2cpm <- Matrix(log2cpm)
+    log2cpm <- Matrix(log2cpm)
     meta    <- readRDS(sprintf("data/%s_sc_label.rds", cell_type))
     nonzero <- rownames(log2cpm)[
        which(rowSums(log2cpm > 0) > 10)
@@ -114,8 +116,6 @@ if (!file.exists(data_file)) {
     nonzero_bcell, nonzero_fibro, nonzero_mono, nonzero_tcell
   ))
   save.image(data_file)
-} else {
-  load(data_file)
 }
 
 # all_cell_types <- cbind.data.frame(log2cpm_fibro, log2cpm_bcell, log2cpm_tcell, log2cpm_mono)
@@ -214,6 +214,7 @@ plot_tsne <- function(log2cpm, dat, marker) {
     plots = list(p1, p2), ncol = 2
   )
 }
+# plot_tsne(log2cpm_fibro, meta_fibro, "CD3D")
 
 plot_box <- function(log2cpm_marker, dat, marker) {
   #dat$marker <- as.numeric(log2cpm[marker,])
@@ -297,6 +298,16 @@ plot_box <- function(log2cpm_marker, dat, marker) {
 
 # User interface --------------------------------------------------------------
 
+# Call this function with all the regular navbarPage() parameters, plus a text parameter,
+# if you want to add text to the navbar
+navbarPageWithText <- function(..., text) {
+  navbar <- navbarPage(...)
+  textEl <- tags$div(class = "navbar-text pull-right", text)
+  navbar[[3]][[1]]$children[[1]] <- htmltools::tagAppendChild(
+    navbar[[3]][[1]]$children[[1]], textEl)
+  navbar
+}
+
 ui <- fluidPage(
   
   tags$head(
@@ -308,7 +319,7 @@ ui <- fluidPage(
   ),
   
   # Application title
-  navbarPage(
+  navbarPageWithText(
     "AMP Phase I",
     
     tabPanel(
@@ -317,7 +328,7 @@ ui <- fluidPage(
       # h3("Integration of Single-cell Transcriptomic and Proteomic 
       #    Immune Profiling Identifies Pathogenic Pathways in Rheumatoid Arthritis"),
       
-      h3("This site is used to explore the AMP RA single-cell RNA-seq clustering analysis and  results."),
+      p("Explore gene expression in single-cell RNA-seq clusters."),
       br(),
       
       tabsetPanel(
@@ -384,6 +395,7 @@ ui <- fluidPage(
                 h4("The expression of selected gene across all the subsets (in violinplot):"),
                 plotOutput("box_marker_plot_all", height = "500px"),
                 br()
+                
               )
             )
             
@@ -456,8 +468,9 @@ ui <- fluidPage(
         
       ) # mainPanel
       
-    ) # tabPanel
+    ), # tabPanel
     
+    text = textOutput("mem_used")
   ) # navbarPage
   
 ) # fluidPage
@@ -487,14 +500,14 @@ server <- function(input, output) {
     )
   })
   
-  output$marker_heatmap <- renderD3heatmap({
-    markers <- get(sprintf("markers_%s", input$cell_type))
-    d3heatmap(
-      x               = -log10(markers),
-      colors          = "Greys",
-      yaxis_font_size = "14px"
-    )
-  })
+  # output$marker_heatmap <- renderD3heatmap({
+  #   markers <- get(sprintf("markers_%s", input$cell_type))
+  #   d3heatmap(
+  #     x               = -log10(markers),
+  #     colors          = "Greys",
+  #     yaxis_font_size = "14px"
+  #   )
+  # })
   
   output$box_marker_plot_single <- renderPlot({
     log2cpm <- get(sprintf("log2cpm_%s", input$cell_type))
@@ -539,6 +552,10 @@ server <- function(input, output) {
       dat     = dat,
       marker  = marker
     )
+  })
+  
+  output$mem_used <- renderText({
+    gdata::humanReadable(pryr::mem_used(), standard = "SI")
   })
   
 }
