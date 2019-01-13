b44819261b58a5449d065d880477e90055aa9a08
Kamil Slowikowski <kslowikowski@gmail.com>
2019-01-12 19:53:49 -0500

    new plot for bulk RNA-seq

--- R/plot-bulk-dots.R
+++ R/plot-bulk-dots.R
@@ -1,73 +1,83 @@
 plot_bulk_dots <- function(dat, marker = "") {
-  
-  # bulk.summary <- aggregate(marker ~ cell_type + inflamed, mean, data=dat)
-  dat_median <- dat %>% group_by(cell_type) %>% group_by(inflamed) %>% summarise(median = median(marker))
-
-  ggplot(data    = dat,
-         mapping = aes(
-           x = cell_type, y = dat$marker, fill = inflamed, group = inflamed
-         )) +
-    # geom_boxplot() +
-    # geom_violin() +
-    # geom_jitter(height = 0, width = 0.25, color = "dimgrey", size = 0.3) +
-    # geom_quasirandom(
-    # geom_sina(
-    geom_jitter(
-      shape = 21, stroke = 0.15, size = 3.5,
-      position = position_jitterdodge(jitter.width = 0.2, seed = 1, dodge.width = 0.8)
-    ) +
-    # geom_segment(data = bulk.summary, aes(x=cell_type,xend=max(pos),y=0,yend=0),colour="green") + 
-    stat_summary(
-      fun.y = median, fun.ymin = median, fun.ymax = median,
-      geom = "crossbar",
-      position = position_jitterdodge(jitter.width = 0.2, seed = 1, dodge.width = 0.8),
-      width = 0.8, size = 0.5,
-      colour = "black",
-      show.legend = FALSE
-      # colour = rep(c("#FF7F00", "#FFD8B2", "#6A3D9A"), 4)
-    ) +
-    # geom_boxplot(position = position_dodge(.75)) + 
-    # geom_hline(data = bulk.summary, aes(yintercept = marker, width = 0.3), 
-    #            col = rep(c("#6A3D9A", "#FFD8B2", "#FF7F00"), 4)) +
-    # geom_crossbar(data=bulk.summary,aes(x = c(0.5, 0, -0.5) + cell_type, 
-    #                                     y = marker,
-    #                                     ymax = marker,
-    #                                     ymin = marker),
-    #                size = 0.3, col = rep(c("#6A3D9A", "#FFD8B2", "#FF7F00"), 4), width = 0.3) +
-    geom_vline(xintercept = c(1,2,3) + 0.5, size = 0.1) +
-    scale_y_continuous(breaks = scales::extended_breaks(n = 4)) +
-    scale_x_discrete(limits = rev(levels(dat$cell_type))) +
-    # geom_vline(
-    #   data = data.frame(x = c(7.5, 11.5, 15.5)),
-    #   mapping = aes(xintercept = x),
-    #   color = "grey70"
-    # ) +
-    coord_flip() +
-    labs(
-      x     = NULL,
-      y     = bquote("Log"[2]~"(TPM+1)"),
-      title = marker
-      # subtitle = tsne_subtitle
-    ) +
-    scale_fill_manual(
-      values = meta_colors$inflamed, name = NULL
-    ) +
-    guides(
-      fill = guide_legend(
-        override.aes = list(size = 4),
-        reverse = TRUE
-      )
-    ) +
-    theme_bw(base_size = 26) + theme(
-      # legend.position = "none",
-      legend.key.size = unit(1.5, "lines"),
-      axis.ticks      = element_line(size = 0.5),
-      # axis.text.y     = element_blank(),
-      axis.ticks.y    = element_blank(),
-      # axis.text       = element_blank(),
-      # axis.ticks      = element_blank(),
-      panel.grid      = element_blank(),
-      panel.border    = element_rect(size = 0.5),
-      plot.title      = element_text(size = 26, face = "italic")
+  ps <- sapply(unique(dat$cell_type), function(i) {
+    kruskal.test(formula = marker ~ inflamed, data = subset(dat, cell_type == i))$p.value
+  })
+  ps <- sprintf("p = %s", format.pval(ps, 1))
+  ps[2] <- sprintf("Kruskal-Wallis, %s", ps[2])
+  dat_p <- data.frame(
+    cell_type = factor(
+      x = unique(as.character(dat$cell_type)),
+      levels = levels(dat$cell_type)
+    ),
+    pvalue = ps
+  )
+  dat$inflamed <- factor(
+    x = as.character(dat$inflamed),
+    levels = c("blank1", levels(dat$inflamed))
+  )
+  # dat <- b_meta
+  ggplot() +
+  geom_boxplot(
+    data    = dat,
+    mapping = aes(
+      x = inflamed, y = marker, fill = inflamed, group = inflamed
+    ),
+    color = "grey20",
+    alpha = 0.4, size = 0.4, width = 0.8, outlier.alpha = 0
+  ) +
+  geom_point(
+    data    = dat,
+    mapping = aes(
+      x = inflamed, y = marker, fill = inflamed, group = inflamed
+    ),
+    shape = 21, size = 2.5,
+    # position = position_jitterdodge(jitter.width = 0.9)
+    position = position_quasirandom()
+  ) +
+  facet_grid(cell_type ~ ., switch = "both") +
+  scale_x_discrete(
+    limits = c("OA", "leukocyte-poor RA", "leukocyte-rich RA", "blank1")
+  ) +
+  scale_y_continuous(
+    breaks = scales::extended_breaks(n = 4)
+  ) +
+  coord_flip() +
+  labs(
+    x     = NULL,
+    y     = bquote("Log"[2]~"(TPM+1)"),
+    title = marker
+    # subtitle = tsne_subtitle
+  ) +
+  scale_fill_manual(
+    values = meta_colors$inflamed, name = NULL
+  ) +
+  geom_text(
+    data = dat_p, size = 6,
+    mapping = aes(x = 4, y = 0, hjust = 0, label = pvalue)
+  ) +
+  guides(
+    fill = guide_legend(keyheight = unit(3, "lines"),
+      override.aes = list(size = 0.5, alpha = 0.9),
+      reverse = TRUE
     )
+  ) +
+  theme_bw(base_size = 26) + theme(panel.spacing.y = unit(0.5, "lines"),
+    strip.text.y = element_text(angle = 180, hjust = 1),
+    strip.background = element_blank(),
+    legend.box.spacing = unit(0.1, "lines"),
+    # legend.position = "none",
+    legend.key.size = unit(1.5, "lines"),
+    axis.ticks      = element_line(size = 0.5),
+    axis.text.y     = element_blank(),
+    axis.ticks.y    = element_blank(),
+    # axis.text       = element_blank(),
+    # axis.ticks      = element_blank(),
+    panel.grid      = element_blank(),
+    panel.border    = element_rect(size = 0.5),
+    plot.title      = element_text(size = 26, face = "italic")
+  )
 }
+# marker <- "THY1"
+# b_meta$marker <- as.numeric(b_log2tpm[marker,])
+# plot_bulk_dots(b_meta, marker)
+
